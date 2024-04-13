2b7145c81c2da3a536830055abea5a9f61522927
Pohi11 <60932567+Pohi11@users.noreply.github.com>
2024-04-13 18:42:03 -0400

    updated file

--- SQL/dml.sql
+++ SQL/dml.sql
@@ -1,10 +1,3 @@
-DO $$DECLARE
-    r RECORD;
-BEGIN
-    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'public') LOOP
-        EXECUTE 'DELETE FROM ' || quote_ident(r.tablename);
-    END LOOP;
-END$$;
 
 BEGIN;
 
